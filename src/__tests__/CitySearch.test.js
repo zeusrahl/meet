4f759abcaab751fbf8d236ecfad0132be76e5fd6
Ethan Dotson-Kelly <83192779+zeusrahl@users.noreply.github.com>
2021-11-14 12:07:22 -0500

    update/bug fixes

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -6,16 +6,10 @@ import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
   let locations, CitySearchWrapper;
-<<<<<<< HEAD
-  beforeAll(() => {
-    locations = extractLocations(mockData);
-    CitySearchWrapper = shallow(<CitySearch locations={locations} />);
-=======
   
   beforeAll(() => {
     locations = extractLocations(mockData);
     CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => { }} />);
->>>>>>> parent of f8bfc8b (Updates)
   });
 
   test('render text input', () => {
@@ -70,8 +64,6 @@ describe('<CitySearch /> component', () => {
     CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
     expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
   });
-<<<<<<< HEAD
-=======
 
   test("selecting CitySearch input reveals the suggestions list", () => {
     CitySearchWrapper.find('.city').simulate('focus');
@@ -88,5 +80,4 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
     expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none' });
   });
->>>>>>> parent of f8bfc8b (Updates)
 })

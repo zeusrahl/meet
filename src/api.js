6c9a3a832aaee6fd29ff7cc1a992f55ee5923d2e
Ethan Dotson-Kelly <83192779+zeusrahl@users.noreply.github.com>
2021-12-03 21:13:51 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ const checkToken = async (accessToken) => {
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
     .then((res) => res.json())
-    .catch((error) => error.json());
+    .catch((error) => console.log(error));
 
   return result;
 }

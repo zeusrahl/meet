80d467cfc80a8601ee59072a29d1eef0391b04fa
Ethan Dotson-Kelly <83192779+zeusrahl@users.noreply.github.com>
2021-12-05 16:00:31 -0500

    Revert "Updating Privacy & Welcome Screen"
    This reverts commit 58ef45b0bf314b3fbd43069c6457574c3c3444c6.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import WelcomeScreen from './WelcomeScreen';
-import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
+import { extractLocations, getEvents } from './api';
 import './nprogress.css';
 
 class App extends Component {
@@ -15,27 +14,19 @@ class App extends Component {
     currentLocation: "all",
     locations: [],
     numberOfEvents: 32,
-    showWelcomeScreen: undefined
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false: true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get('code');
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          const filteredEvents = events.slice(0, this.state.numberOfEvents);
-          this.setState({
-            events: filteredEvents,
-            locations: extractLocations(events),
-          });
-        }
-      });
-    }
+    getEvents().then((events) => {
+      if (this.mounted) {
+        const filteredEvents = events.slice(0, this.state.numberOfEvents);
+        this.setState({
+          events: filteredEvents,
+          locations: extractLocations(events),
+        });
+      }
+    });
   }
 
   componentWillUnmount(){
@@ -64,7 +55,7 @@ class App extends Component {
 
   render() {
     const { locations, events, numberOfEvents } = this.state;
-    if (this.state.showWelcomeScreen === undefined) return (
+    return (
       <Container className="App">
         <Row>
           <Col className="CitySearchWrapper" md={6}>
@@ -79,7 +70,6 @@ class App extends Component {
             <EventList events={events}/>
           </Col>
         </Row>
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </Container>
     ); 
   }
